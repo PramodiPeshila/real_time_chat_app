ec4879c7e2ccb9eecc8a4973ea5a44ad753fa6a3
Pramodi Peshila <ppeshila7@gmail.com>
2025-07-26 23:36:44 +0530

    dev-02| created authentication file for navigation

---
+++ lib/pages/Authenticate.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:realtime_chat_app/pages/HomeScreen.dart';
+import 'package:realtime_chat_app/pages/LogginScreen.dart';
+
+class Authenticate extends StatelessWidget {
+  const Authenticate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    
+    return StreamBuilder<User?>(
+      stream: auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        
+        if (snapshot.hasData && snapshot.data != null) {
+          return const HomeScreen(); // Navigate to home screen if user is logged in
+        } else {
+          return const Logginscreen(); // Show login screen if user is not logged in
+        }
+      },
+    );
+  }
+}
