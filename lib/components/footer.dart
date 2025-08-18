50a532b0363a8b0ed7d7ee60de6305cf566eae79
Pramodi Peshila <ppeshila7@gmail.com>
2025-08-18 16:43:01 +0530

    dev-2| created new profile screen

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:realtime_chat_app/pages/profile.dart';
+import 'package:realtime_chat_app/pages/profile_screen.dart';
 import 'package:realtime_chat_app/pages/qr_genetator.dart';
 import 'package:realtime_chat_app/pages/home_screen.dart';
 import 'package:realtime_chat_app/pages/contacts_screen.dart';
