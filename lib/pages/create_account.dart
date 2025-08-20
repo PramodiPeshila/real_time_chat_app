91d8ff02423ffe3f4fa98e56ff2e741928ec9f45
R.K.Lakshan <lakshan2725@gmail.com>
2025-08-20 07:24:49 +0530

    [dev-01]| changed background color, add icon

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -19,84 +19,119 @@ class _CreateAccountState extends State<CreateAccount> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: isLoading ?
-      Center(
-        child: SizedBox(
-          width: size.width / 20,
-          height: size.width / 20,
-          child: const CircularProgressIndicator(),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color.fromARGB(255, 255, 255, 255), Color(0xFF42A5F5)],
+          ),
         ),
-      ) :
-      SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: size.height / 20),
-            Container(
-              alignment: Alignment.centerLeft,
-              width: size.width / 1.2,
-              child: IconButton(
-                icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black),
-                onPressed: () => Navigator.pop(context),
-              ),
-            ),
-            SizedBox(height: size.height / 50),
-            SizedBox(
-              width: size.width / 1.3,
-              child: const Text(
-                "Welcome",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(
-              width: size.width / 1.3,
-              child: const Text(
-                "Create Account to continue",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 141, 141, 141),
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
+        child: isLoading
+            ? Center(
+                child: SizedBox(
+                  width: size.width / 20,
+                  height: size.width / 20,
+                  child: const CircularProgressIndicator(),
                 ),
-              ),
-            ),
-            SizedBox(height: size.height / 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 18.0),
-              child: Container(
-                width: size.width,
-                alignment: Alignment.center,
-                child: _buildField(size, "Name", Icons.account_box, _name),
-              ),
-            ),
-            Container(
-              width: size.width,
-              alignment: Alignment.center,
-              child: _buildField(size, "Email", Icons.email, _email),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 18.0),
-              child: Container(
-                width: size.width,
-                alignment: Alignment.center,
-                child: _buildField(size, "Password", Icons.lock, _password),
-              ),
-            ),
-            SizedBox(height: size.height / 20),
+              )
+            : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(height: size.height / 10),
 
-            _buildCustomButton(size),
-            SizedBox(height: size.height / 20),
-            GestureDetector(
-              onTap: () => Navigator.pop(context),
-              child: const Text(
-                "Login",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
+                    SizedBox(height: size.height / 20),
+                    Container(
+                      child: Image.asset(
+                        "lib/assets/icon.png",
+                        width: 150,
+                        height: 150,
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width / 1.3,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Sign up to LinkTalk",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width / 1.3,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "first time here?",
+                            style: TextStyle(
+                              color: Colors.blueGrey,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () => Navigator.pop(context),
+                            child: const Text(
+                              " Login",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: size.height / 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 18.0),
+                      child: Container(
+                        width: size.width,
+                        alignment: Alignment.center,
+                        child: _buildField(
+                          size,
+                          "Name",
+                          Icons.account_box,
+                          _name,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: size.width,
+                      alignment: Alignment.center,
+                      child: _buildField(size, "Email", Icons.email, _email),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 18.0),
+                      child: Container(
+                        width: size.width,
+                        alignment: Alignment.center,
+                        child: _buildField(
+                          size,
+                          "Password",
+                          Icons.lock,
+                          _password,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height / 30),
+
+                    _buildCustomButton(size),
+                    
+                  ],
                 ),
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
@@ -105,16 +140,19 @@ class _CreateAccountState extends State<CreateAccount> {
     return GestureDetector(
       onTap: () async {
         if (_name.text.isNotEmpty &&
-            _email.text.isNotEmpty && 
+            _email.text.isNotEmpty &&
             _password.text.isNotEmpty) {
-
           setState(() {
             isLoading = true;
           });
 
           try {
-            final user = await createAccount(_name.text, _email.text, _password.text);
-            
+            final user = await createAccount(
+              _name.text,
+              _email.text,
+              _password.text,
+            );
+
             setState(() {
               isLoading = false;
             });
@@ -144,14 +182,14 @@ class _CreateAccountState extends State<CreateAccount> {
         height: size.height / 14,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          color: Colors.blue,
+          color: const Color.fromARGB(255, 0, 94, 255),
         ),
         alignment: Alignment.center,
         child: const Text(
-          "Create Account",
+          "Sign Up",
           style: TextStyle(
             color: Colors.white,
-            fontSize: 22,
+            fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -159,7 +197,12 @@ class _CreateAccountState extends State<CreateAccount> {
     );
   }
 
-  Widget _buildField(Size size, String hintText, IconData icon, TextEditingController cont) {
+  Widget _buildField(
+    Size size,
+    String hintText,
+    IconData icon,
+    TextEditingController cont,
+  ) {
     return SizedBox(
       width: size.width / 1.3,
       height: size.height / 15,
@@ -170,9 +213,7 @@ class _CreateAccountState extends State<CreateAccount> {
           prefixIcon: Icon(icon),
           hintText: hintText,
           hintStyle: const TextStyle(color: Colors.grey),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
         ),
       ),
     );
@@ -181,10 +222,7 @@ class _CreateAccountState extends State<CreateAccount> {
   void _showErrorMessage(String message) {
     if (mounted) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(message),
-          backgroundColor: Colors.red,
-        ),
+        SnackBar(content: Text(message), backgroundColor: Colors.red),
       );
     }
   }
